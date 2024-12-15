30d57a39846d989bb6a254ef3a215d136f31ce3e
Sudhanshu Kumar Jha <113638708+Sudhanshujha10@users.noreply.github.com>
2024-12-15 09:36:19 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I am a passionate fresher with strong software development skills. - Let's bring real-world projects to life. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "If not coding, I'm traveling and exploring new places",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Hotel Listing Application",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "ShareMe",
+    des: "A Modern Social Media Platform where u can engage urself to others by creating your posts, likes and comments.",
+    img: "/Shareme.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    href: "https://github.com/Sudhanshujha10/Shareme.git",
+  },
+  {
+    id: 2,
+    title: "Fake News Detection",
+    des: "A machine learning project for predicting fake news with an accuracy of 91%",
+    img: "/FNDS.png",
+    iconLists: ["/python.svg", "/jupyter.svg"],
+    href: "https://github.com/Sudhanshujha10/Fake-news-Detection-System.git",
+  },
+  {
+    id: 3,
+    title: "AI-Chatbot",
+    des: "An Open-Source AI Chatbot Template Built With Next.js and the AI SDK by Vercel.",
+    img: "/giphy.webp",
+    iconLists: ["/re.svg", "/tail.svg", "/vercel.svg", "/next.svg"],
+    href: "https://github.com/Sudhanshujha10/ai-chatbot.git",
+  },
+  {
+    id: 4,
+    title: "Sudoku Solver",
+    des: "Created a Sudoku Solver game, using core undersatnding of backtracking and problem solving skills through a visualisation using a sudoku game.",
+    img: "/Sudokugif.gif",
+    iconLists: ["/html.svg", "/css.svg", "/cpp.svg", "/js.svg"],
+    href: "https://sudhanshujha10.github.io/Sudoku/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Sudhanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudhanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudhanshu is the ideal partner.",
+    name: "",
+    title: "Developer Community, WebD",
+  },
+  {
+    quote:
+      "Collaborating with Sudhanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudhanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudhanshu is the ideal partner.",
+    name: "",
+    title: "Developer Community, WebD",
+  },
+  {
+    quote:
+      "Collaborating with Sudhanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudhanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudhanshu is the ideal partner.",
+    name: "",
+    title: "Developer Community, WebD",
+  },
+  {
+    quote:
+      "Collaborating with Sudhanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudhanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudhanshu is the ideal partner.",
+    name: "",
+    title: "Developer Community, WebD",
+  },
+  {
+    quote:
+      "Collaborating with Sudhanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudhanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudhanshu is the ideal partner.",
+    name: "",
+    title: "Developer Community, WebD",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern at Wictrinix",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Project Team Lead",
+    desc: "Designed and developed a web based result management platform for university students.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Reasearch in Machine Learning",
+    desc: "Worked in team to create a machine learning model for predicting fake news with an accuracy of 91%.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Front-End freelancing",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/Sudhanshujha10",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    href: "https://x.com/sudhanshuJha02",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/sudhanshu-kumar-jha-d10m10y2002/",
+  },
+];
